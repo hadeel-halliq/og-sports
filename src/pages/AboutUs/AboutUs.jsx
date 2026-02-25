d41b77656da1bb11b433dbdefef09db7c7407857
hadeel-halliq <hadeelhalliq@gmail.com>
2026-02-25 15:44:52 -0800

    add Statistics section to about page

--- src/pages/AboutUs/AboutUs.jsx
+++ src/pages/AboutUs/AboutUs.jsx
@@ -1,11 +1,13 @@
 import HeroSection from "./components/HeroSection";
 import OurStory from "./components/OurStory";
+import Statistics from "./components/Statistics";
 
 export default function AboutUs() {
     return (
         <div className="z-20">
             <HeroSection/>
             <OurStory/>
+            <Statistics/>
         </div>
     )
 }
